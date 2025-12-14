3af53410d7ed68f952d5fd19522e9bd76da48001
DanielWijayaa <danielwijaya_2226250060@mhs.mdp.ac.id>
2025-12-14 17:25:30 +0700

    push

--- School_Simulation_java-main_After/class_pac/Senior.java
+++ School_Simulation_java-main_After/class_pac/Senior.java
@@ -1,5 +1,6 @@
 ///////////////////////////
 // Implementation of Senior
+// (Liskov Substitution Principle)
 ///////////////////////////
 
 
@@ -8,24 +9,35 @@ package class_pac;
 
 
 // Senior
+// Kelas Senior dapat menggantikan Student
+// TANPA mengubah perilaku yang diharapkan
+// -> memenuhi Liskov Substitution Principle
 public class Senior extends Student {
-    
+
     // Constructor
     public Senior(String name, int nf, int nc) {
-        // Initialization of super-class
+        // Inisialisasi superclass
         super(name, nf, nc);
 
         System.out.println("A New Senior has been created!");
     }
 
     // Attend Senior
+    // Method ini memiliki:
+    // - Nama method sama
+    // - Parameter sama
+    // - Tidak mengubah kontrak Student
+    // - Tidak melempar exception baru
+    // -> Senior bisa menggantikan Student di mana pun
     @Override
     public void attend(int N, int L) {
-        // Add N * Ls
+
+        // Logika attend tetap masuk akal
+        // Tidak mengurangi perilaku yang dijanjikan Student
         this.tired += (N * L);
     }
 
     // Print
-    // It just inherits the print from Person
-    // (it does the same thing)
+    // Tidak dioverride karena perilakunya sama
+    // Tetap mengikuti kontrak Person
 }
