5263559f5d83c0ad20a9fa62d15b8151583e00fd
DanielWijayaa <danielwijaya_2226250060@mhs.mdp.ac.id>
2025-12-14 17:16:42 +0700

    push

--- School_Simulation_java-main_After/class_pac/School.java
+++ School_Simulation_java-main_After/class_pac/School.java
@@ -2,13 +2,14 @@
 // Implementation of School
 ///////////////////////////
 
-
+// SRP
 // Naming package
 package class_pac;
 
 
 // School
 public class School {
+
     private Floor[] floors; // Array of Floors
     private Yard yard;      // Schoolyard
     private Stairs stairs;  // Stairs
@@ -17,8 +18,10 @@ public class School {
     private int Ls;         // Units for Senior
     private int Lt;         // Units for Teacher
 
+
     // Constructor
     public School(int Lj, int Ls, int Lt, int Cclass) {
+
         // Initialization
         this.Lj = Lj;
         this.Ls = Ls;
@@ -26,51 +29,70 @@ public class School {
 
         yard = new Yard();
         stairs = new Stairs();
+
         floors = new Floor[3];
-        for (int i=0 ; i<3 ; ++i) floors[i] = new Floor(i, Cclass);
+        for (int i = 0; i < 3; ++i) {
+            floors[i] = new Floor(i, Cclass);
+        }
 
         System.out.println("A New School has been created!");
     }
 
+
     // Enter student in school
+    // SRP: School only manages the school, not student behavior
     public void enter(Student s) {
+
         System.out.println(s.get_name() + " enters school");
 
-        // Get Student in yard, then stairs and finally in his floor
+        // Yard
         yard.enter(s);
         s = yard.exit();
 
+        // Stairs
         stairs.enter(s);
         s = stairs.exit();
 
+        // Floor
         floors[s.get_flo()].enter(s);
     }
 
+
     // Place teacher in school
     public void place(Teacher t) {
-        this.floors[t.get_flo()].place(t);
+        floors[t.get_flo()].place(t);
     }
+
 
     // Operate school
+    // SRP: operation logic delegated to Floor
     public void operate(int N) {
-        for (int i=0 ; i<3 ; ++i) floors[i].operate(N, Lj, Ls, Lt);
+
+        for (int i = 0; i < 3; ++i) {
+            floors[i].operate(N, Lj, Ls, Lt);
+        }
     }
 
-    // Print
+
+    // Print school status
     public void print() {
+
         System.out.println("School life consists of:");
 
-        for (int i=0 ; i<3 ; ++i) floors[i].print();
+        for (int i = 0; i < 3; ++i) {
+            floors[i].print();
+        }
     }
+
 
     // Empty school
     public void empty() {
-        // Empty Students
-        for (int i=0 ; i<3 ; ++i) {
-            for (int k=0 ; k<6 ; ++k) {
-                for (int j=0 ; j<floors[i].get_ccls() ; ++j) {
-                    // Get Student from his floor to stairs
-                    // and then to yard
+
+        // Empty students
+        for (int i = 0; i < 3; ++i) {
+            for (int k = 0; k < 6; ++k) {
+                for (int j = 0; j < floors[i].get_ccls(); ++j) {
+
                     Student s = floors[i].exit(k);
 
                     stairs.enter(s);
@@ -82,10 +104,10 @@ public class School {
             }
         }
 
-        // Empty Teachers
-        for (int i=0 ; i<3 ; ++i) {
-            for (int j=0 ; j<6 ; ++j) {
-                this.floors[i].teacher_out(j);
+        // Empty teachers
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 6; ++j) {
+                floors[i].teacher_out(j);
             }
         }
     }
